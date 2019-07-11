0aded17f91ec7a456a15abccde96039903e26659
Administrator <https://github.com/how2j/hiworld>
2019-07-11 11:52:14 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -2,5 +2,6 @@ public class HiWorld {
     public  static void  main(String[] args){
         System.out.println("Hi World");
         System.out.println("提交项目....");
+        System.out.println("我成功了");
     }
 }
